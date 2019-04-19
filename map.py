f2241eca9d67de9ccd118f62540217a1bc13d2b6
gandra70 <dragan@gmail.com>
2019-04-19 12:44:57 +0200

    'changes'

--- map.py
+++ map.py
@@ -7,14 +7,14 @@ import folium
 m = folium.Map(location=[44.647502, 20.291818], zoom_start=12)
 
 # global tooltip
-tooltip1 = "Belgrade Airport. Click for more info! "
-tooltip2 = "Belgrade Fortress. Click for more info! "
-tooltip3 = "Belgrade Zoo. Click for more info! "
-tooltip4 = "High School of Electrical Engineering 'Nikola Tesla'. Click for more info! "
-tooltip5 = "Belgrade Fair . Click for more info! "
-tooltip6 = "Belgrade Racecourse. Click for more info! "
-tooltip7 = "Forex Stock Exchange Forum. Click for more info! "
-tooltip8 = "Restricted area. Click for more info! "
+tooltip1 = "Click for more info! "
+tooltip2 = "Click for more info! "
+tooltip3 = "Click for more info! "
+tooltip4 = "This is my school. More info! "
+tooltip5 = "Click for more info! "
+tooltip6 = "Click for more info! "
+tooltip7 = "Forex Stock Exchange Forum. More info! "
+tooltip8 = "Restricted area.More info! "
 tooltip9 = "Click for more info! "
 tooltip10 = "Kosutnjak Forest! "
 # icon marker
@@ -27,27 +27,27 @@ visData = os.path.join('data', 'vis.json')
 geoOverlay =os.path.join('data', 'geoOverlay.json')
 # markers
 folium.Marker([44.818181, 20.294455],
-              popup="Belgrade Nikola Tesla Airport is an international airport serving Belgrade. It is the largest airport in Serbia, situated 18 km west of downtown.",
+              popup="'Nikola Tesla' International  Airport",
               tooltip=tooltip1,
               icon=folium.Icon(color="blue", icon="cloud")).add_to(m),
 folium.Marker([44.823360,20.450313],
-              popup="Belgrade Fortress, consists of the old citadel and Kalemegdan Park on the confluence of the River Sava and Danube, in an urban area of modern Belgrade, the capital of Serbia. It is located in Belgrade's municipality of Stari Grad.",
+              popup="Belgrade Fortress",
               tooltip=tooltip2,
               icon=folium.Icon(color="blue", icon="cloud")).add_to(m),
 folium.Marker([44.825597,20.454002],
-              popup="Beo zoo vrt, also known as Vrt dobre nade, is a zoo located in Kalemegdan Park, downtown of Belgrade, Serbia. Considered to be one of the oldest public zoos in southeastern Europe, it was founded in 1936 and is the biggest zoo in the country.",
+              popup="Zoo",
               tooltip=tooltip3,
               icon=folium.Icon(color="green", icon="cloud")).add_to(m),
 folium.Marker([44.810311,20.460782],
-              popup="This is my school",
+              popup="'Nikola Tesla' High School",
               tooltip=tooltip4,
               icon=folium.Icon(color="blue", icon="cloud")).add_to(m),
 folium.Marker([44.796171, 20.437084],
-              popup="The Belgrade Fair is a large complex of three large domes and a dozen of smaller halls. It is located in the municipality of Savski Venac, on the right bank of the Sava river. ",
+              popup="The Belgrade Fair. ",
               tooltip=tooltip5,
               icon=folium.Icon(color="blue", icon="cloud")).add_to(m),
 folium.Marker([44.786568, 20.427067],
-              popup="Racecourse at Careva Cuprija is the oldest sports facility in Belgrade. By the Decree of King Alexander dated 15th November 1920, the Danube Riders’ Circle Knez Mihajlo was alloted the state land free of lease for a period of 75 years, for the purpose of fulfilling the need for the permanent racetrack for public horse games.",
+              popup="Hippodrome / Racecourse  ",
               tooltip=tooltip6,
               icon=folium.Icon(color="blue", icon="cloud")).add_to(m),
 folium.Marker([44.814877, 20.455335],
@@ -57,12 +57,11 @@ folium.Marker([44.814877, 20.455335],
 folium.CircleMarker(
               location=[44.770493,20.469921],
               radius=30,
-              popup="The Military Academy of the University of Defence is a training college devoted to military education and career development located in Belgrade, Serbia. ",
+              popup="The Military Academy of the University of Defence.",
               tooltip=tooltip8,
               color='#fc5628',
               fill=True,
-              fill_color='#fc5628',
-              icon=Logo1).add_to(m),
+              fill_color='#fc5628').add_to(m),
 # vis marker
 folium.Marker(
               location=[44.829768, 20.433458],
